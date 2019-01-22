4844babeb7cf608971d4ef1d6663a6324ea871ed
vpatrin <victor.patrin150@gmail.com>
2019-01-22 18:12:37 +0100

    update args

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ubirch-python-utils",
-    version="1.2.8",
+    version="1.2.9",
     author="Victor Patrin",
     author_email="victor.patrin150@gmail.com",
     description="A python utils ubirch for ubirch anchoring services.",

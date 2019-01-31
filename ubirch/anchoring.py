2b8ab85e90fe5e53da602bd08f944ad9757c265b
vpatrin <victor.patrin150@gmail.com>
2019-01-31 11:42:25 +0100

    added queue names args

--- ubirch/anchoring.py
+++ ubirch/anchoring.py
@@ -51,11 +51,19 @@ def set_arguments(service):
         parser.add_argument('-seed', '--seed', help='IOTA seed, default is None', metavar='IOTA SEED',
                             type=str, default=None)
 
+    # SQS queues & Kafka topics
+    parser.add_argument('-i', '--input', help='Kafka topic receiving input messages not anchored yet',
+                        metavar='INPUT', default='input_messages')
+    parser.add_argument('-o', '--output', help='Kafka topic receiving'
+                                               'JSON documents: {status:added, message:m, tx_hash: txic} ',
+                        metavar='OUTPUT', default='output_messages')
+    parser.add_argument('-e', '--errors', help='Kafka topic receiving error messages',
+                        metavar='ERRORS', default='error_messages')
 
     # KAFKA config
-    parser.add_argument('-p', '--port',
-                        help="port of the producer or consumer, default is 9092",
-                        metavar="KAFKA PORT", default='localhost:9092')
+    parser.add_argument('-bs', '--bootstrap_server',
+                        help="host:port for kafka consumer/producer, default is localhost:9092",
+                        metavar="KAFKA_BOOTSTRAP_SERVER", default='localhost:9092')
     # SQS config
     parser.add_argument('-u', '--url',
                         help="endpoint url of the sqs server, input localhost:9324 for local connection (default)",
@@ -135,7 +143,7 @@ def send(message, server, queue=None, topic=None, producer=None):
         producer.flush()
 
 
-def process_message(message, server, error_queue, queue2, store_function, producer):
+def process_message(message, server, error_messages, output_messages, store_function, producer):
     """
     Anchors the message m in a DLT specified by the store_function parameter.
         Sends error (non hex message and timeouts) in the errorQueue.
@@ -145,8 +153,8 @@ def process_message(message, server, error_queue, queue2, store_function, produc
 
     :param message: message to anchor in the blockchain or tangle
     :param server: choice between 'SQS' or 'KAFKA' : the choice of the messaging service
-    :param error_queue: SQS queue or KAFKA topic, depending on the server choice
-    :param queue2: SQS queue or KAFKA topic, depending on the server choice
+    :param error_messages: SQS queue or KAFKA topic, depending on the server choice
+    :param output_messages: SQS queue or KAFKA topic, depending on the server choice
     :param store_function: function depending on the blockchain or tangle used (IOTA, Multichain, Ethereum)
     :param producer: Kafka producer used to 'produce' messages in a topic
 
@@ -155,24 +163,24 @@ def process_message(message, server, error_queue, queue2, store_function, produc
     storing_result = store_function(message)
     if not storing_result:
         json_error = json.dumps({"Not a hash": message})
-        send(json_error, server, queue=error_queue, topic='error_queue', producer=producer)
+        send(json_error, server, queue=error_messages, topic=error_messages, producer=producer)
 
     elif storing_result['status'] == 'timeout':  # For Ethereum
         json_error = json.dumps(storing_result)
-        send(json_error, server, queue=error_queue, topic='error_queue', producer=producer)
+        send(json_error, server, queue=error_messages, topic=error_messages, producer=producer)
 
     else:
         json_data = json.dumps(storing_result)
-        send(json_data, server, queue=queue2, topic='queue2', producer=producer)
+        send(json_data, server, queue=output_messages, topic=output_messages, producer=producer)
 
 
-def poll(queue1, error_queue, queue2, store_function, server, producer):
+def poll(input_messages, error_messages, output_messages, store_function, server, producer):
     """
-    Polls queue1 (kafka topic or SQS queue, depending on the server parameter) and process its messages
+    Polls input_messages (kafka topic or SQS queue, depending on the server parameter) and process its messages
 
-    :param queue1: SQS queue or KAFKA topic, depending on the server choice
-    :param error_queue: SQS queue or KAFKA topic, depending on the server choice
-    :param queue2: SQS queue or KAFKA topic, depending on the server choice
+    :param input_messages: SQS queue or KAFKA topic, depending on the server choice
+    :param error_messages: SQS queue or KAFKA topic, depending on the server choice
+    :param output_messages: SQS queue or KAFKA topic, depending on the server choice
 
     :param store_function: Choice of the type of service between IOTA, MultiChain or Ethereum
     :param server: choice betweeen 'SQS' or 'KAFKA' : the choice of the messaging service
@@ -180,15 +188,15 @@ def poll(queue1, error_queue, queue2, store_function, server, producer):
 
     """
     if server == 'SQS':
-        messages = queue1.receive_messages()
+        messages = input_messages.receive_messages()
         for m in messages:
             message = m.body
-            process_message(message, server, error_queue, queue2, store_function, producer)
+            process_message(message, server, error_messages, output_messages, store_function, producer)
             m.delete()
 
     elif server == 'KAFKA':
-        for m in queue1:
+        for m in input_messages:
             message = m.value.decode('utf-8')
-            process_message(message, server, error_queue, queue2, store_function, producer)
+            process_message(message, server, error_messages, output_messages, store_function, producer)
 
 
